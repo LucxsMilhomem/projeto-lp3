64314e863acd45b2c294dc23e3ae2b6a93c9830c
LucasAM <lucasamilhomem1234@gmail.com>
2024-06-18 08:59:48 -0300

    Integração HTML + Python (Jinja)

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,46 +1,26 @@
-# importa a class Flask do módulo flask
-from flask import Flask
-from validate_docbr import CPF,CNPJ
+from flask import Flask, render_template
 
-# Instancia um objeto flask que representa a
-# aplicação
 app = Flask("Minha Aplicação")
 
-# rota + função
-# rota: definição de um padrão de url
-# função: função python com retorno (string, template, outro)
 
-# página home - /
 @app.route("/")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template("home.html")
 
 # página contato - /contato
 @app.route("/contato")
 def contato():
-    return "<h1>Contato</h1>"
+    return render_template("contato.html")
 
 # página produtos - /produtos
 @app.route("/produtos")
 def produtos():
-    return "<h1>Produtos</h1>"
+    lista_produtos = [
+        { "nome": "Coca-cola", "descricao": "Mata a sede" },   
+        { "nome": "Doritos", "descricao": "Suja a mão" },
+        { "nome": "Chocolate", "descricao": "Bom" }
+    ]
 
-# /gerar-cpf (devolve um cpf aleatorio)
-@app.route("/gerar-cpf")
-def cpf():
-    cpf = CPF()
-    return f"<h1>Gerar CPF</h1><p>CPF: {cpf.generate(mask=True)}"
-
-# /gerar-cnpj (devolve um cnpj aleatorio)
-@app.route("/gerar-cnpj")
-def cnpj():
-    cnpj = CNPJ()
-    return f"<h1>Gerar CNPJ</h1><p>CNPJ: {cnpj.generate(mask=True)}"
-
-# /servicos (devolve um titulo com "Nossos Serviços")
-# /gerar-cnpj (devolve um cnpj aleatorio)
-@app.route("/servicos")
-def servicos():
-    return "<h1>Nossos Serviços</h1>"
+    return render_template("produtos.html", produtos=lista_produtos)
 
 app.run()
